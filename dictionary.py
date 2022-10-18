e6540bd195dc7ee075805e43328a6fcc3403927c
AlisonMazzarella <maz12005@byui.edu>
2022-10-18 10:56:39 +0200

    Created feature to open words.csv and pull random word for guessing game

--- dictionary.py
+++ dictionary.py
@@ -1,1 +1,13 @@
 #dictionary class to open and process words.csv file with randomized words
+
+#using randint()
+import random 
+
+# open file
+with open("words.csv", "r") as file:
+    data = file.read()
+    words = data.split()
+
+    #Generating a random number for word position
+    word_pos = random.randint(0, len(words)-1)
+    print("Your word was:", words[word_pos])
